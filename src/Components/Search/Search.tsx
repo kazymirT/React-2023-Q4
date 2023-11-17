3707634a02644cba91d4d40f80508b5fbccbaeb8
Taras <kazemir.t23.23@gmail.com>
2023-11-17 12:49:41 +0200

    refactor: improve code structure in Search component

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -5,9 +5,11 @@ import { setSearchValue } from '../../Slice/searchSlice';
 import { RootState } from '../../Store/store';
 import { setLocalStorages } from '../Storage/Storage';
 import { updatePage } from '../../Slice/fetchArgSlice';
+import { useHistory } from 'react-router-use-history';
 
 export const Search = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
   const { searchValue } = useSelector((state: RootState) => state.searchValue);
   const [query, setQuery] = useState(searchValue);
 
@@ -15,11 +17,25 @@ export const Search = () => {
     setQuery(searchValue);
   }, [searchValue]);
 
-  const handlerSubmit = useCallback(() => {
-    dispatch(setSearchValue(query));
-    dispatch(updatePage({ page: '1' }));
-    setLocalStorages('search', query);
-  }, [dispatch, query]);
+  const updateUrlPage = useCallback(
+    (newSearch: string) => {
+      const newUrl = new URL(location.toString());
+      newUrl.searchParams.set('search', `${newSearch}`);
+      history.push(newUrl.search);
+    },
+    [history]
+  );
+
+  const handlerSubmit = useCallback(
+    (event: React.ChangeEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      dispatch(setSearchValue(query));
+      dispatch(updatePage({ page: '1' }));
+      setLocalStorages('search', query);
+      updateUrlPage(query);
+    },
+    [dispatch, query, updateUrlPage]
+  );
 
   return (
     <SearchDiv>
